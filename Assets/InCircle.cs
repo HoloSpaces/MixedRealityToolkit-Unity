28269a986f84fa68a93797b24790133d830bc4fd
Alexander Seeck <as@hololux.de>
2019-07-25 23:13:06 +0200

    flattened angles

--- Assets/InCircle.cs
+++ Assets/InCircle.cs
@@ -10,10 +10,13 @@ public class InCircle : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Vector3 cursorPosition = pointer.position + pointer.forward * pointerDistance;
-        Vector3 pointerToCenter = -pointer.position;
+        Vector3 flattendPointerPosition = Vector3.ProjectOnPlane(pointer.position, Vector3.up);
+        Vector3 flattenedPointerForward = Vector3.ProjectOnPlane(pointer.forward, Vector3.up).normalized;
+
+        Vector3 cursorPosition = flattendPointerPosition + flattenedPointerForward * pointerDistance;
+        Vector3 pointerToCenter = -flattendPointerPosition;
         float ponterToCenterDistance = pointerToCenter.magnitude;
-        float pointerForwardToCenterAngle = Vector3.SignedAngle(pointerToCenter, pointer.forward, Vector3.up);
+        float pointerForwardToCenterAngle = Vector3.SignedAngle(pointerToCenter, flattenedPointerForward, Vector3.up);
         float pointerToCenterAngle = Vector3.SignedAngle(pointerToCenter, Vector3.forward, Vector3.up);
 
         float sin = ponterToCenterDistance * Mathf.Sin(pointerForwardToCenterAngle * Mathf.Deg2Rad);
@@ -21,33 +24,34 @@ public class InCircle : MonoBehaviour
         float secondAngle = Mathf.Asin(sin) * Mathf.Rad2Deg;
         float distance = Mathf.Sin((secondAngle + pointerForwardToCenterAngle) * Mathf.Deg2Rad) * pointerToCenter.magnitude / sin;
 
+        Handles.ArrowHandleCap(0, pointer.position, Quaternion.LookRotation(pointer.forward, pointer.up), .5f, EventType.Repaint);
         Handles.DrawWireDisc(Vector3.zero, Vector3.up, 1f);
-        //Handles.DrawLine(pointer.position, Vector3.zero);
-        //Handles.DrawLine(pointer.position, pointer.position + pointer.forward * distance);
-        //Handles.DrawLine(Vector3.zero, pointer.position + pointer.forward * distance);
+        Handles.DrawLine(flattendPointerPosition, Vector3.zero);
+        Handles.DrawLine(flattendPointerPosition, flattendPointerPosition + flattenedPointerForward * distance);
+        Handles.DrawLine(Vector3.zero, flattendPointerPosition + flattenedPointerForward * distance);
 
         Color prevColor = Handles.color;
 
         // We are in cone range
-        //if (Mathf.Abs(sin) <= 1f)
-        //{
-        Handles.color = Color.blue;
-        //    Handles.DrawLine(Vector3.zero, Quaternion.Euler(0f, 180f - secondAngle + pointerForwardToCenterAngle - pointerToCenterAngle, 0f) * Vector3.forward);
-        Handles.DrawLine(pointer.position, cursorPosition);
-        Handles.DrawSolidDisc(cursorPosition, Vector3.up, .05f);
-        //}
+        if (Mathf.Abs(sin) <= 1f)
+        {
+            Handles.color = Color.blue;
+            Handles.DrawLine(Vector3.zero, Quaternion.Euler(0f, 180f - secondAngle + pointerForwardToCenterAngle - pointerToCenterAngle, 0f) * Vector3.forward);
+            Handles.DrawLine(flattendPointerPosition, cursorPosition);
+            Handles.DrawSolidDisc(cursorPosition, Vector3.up, .05f);
+        }
 
         Handles.color = Color.green;
         // The pointer is inside the BoundingBox
-        if (pointer.position.magnitude < 1f)
-        {
-            Handles.DrawSolidDisc(pointer.position + pointer.forward * distance, Vector3.up, .05f);
-        }
 
         // The cursor is inside the BoundingBox
-        else if (cursorPosition.magnitude < 1f)
+        if (cursorPosition.magnitude < 1f)
         {
             Handles.DrawSolidDisc(cursorPosition.normalized, Vector3.up, .05f);
+        }
+        else if(flattendPointerPosition.magnitude < 1f)
+        {
+            Handles.DrawSolidDisc(flattendPointerPosition + flattenedPointerForward * distance, Vector3.up, .05f);
         }
 
         // Pointer and cursor are outside of the BoundingBox
@@ -55,7 +59,7 @@ public class InCircle : MonoBehaviour
         {
             float angle;
 
-            if (Vector3.Dot(pointer.forward, cursorPosition) <= 0f)
+            if (Vector3.Dot(flattenedPointerForward, cursorPosition) <= 0f)
                 angle = 180f - secondAngle;
             else
                 angle = secondAngle;
@@ -64,6 +68,8 @@ public class InCircle : MonoBehaviour
 
             Handles.DrawSolidDisc(Quaternion.Euler(0f, angle, 0f) * Vector3.forward, Vector3.up, .05f);
         }
+
+        // Overextended Rotation
         else
         {
             Handles.DrawSolidDisc(Vector3.zero, Vector3.up, .05f);
