5380ee82dfe34818a985667df949c00951434c54
Alexander Seeck <as@holospaces.com>
2020-04-30 08:32:51 +0200

    added UnityEvent

---
+++ Assets/MRTK/SDK/Features/Input/Events/PointerInterfaceUnityEvent.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine.Events;
+using Microsoft.MixedReality.Toolkit.Input;
+
+namespace Microsoft.MixedReality.Toolkit.UI
+{
+    /// <summary>
+    /// Unity event for a pan / zoom event. Contains the hand pan event data
+    /// </summary>
+    [System.Serializable]
+    public class PointerInterfaceUnityEvent : UnityEvent<IMixedRealityPointer> { }
+}
